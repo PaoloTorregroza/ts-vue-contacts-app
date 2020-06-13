973e2bcb8f58088fc4cd997559669f689313a3c6
PaoloTorregroza <paolodydtorregrosa@gmail.com>
2020-06-13 14:24:32 -0500

    Contact component and basic setup

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-app>
+    <v-app-bar app clipped-left>
+      <v-toolbar-title>Contacts app</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-title class="toolbar-option"><a>Login</a></v-toolbar-title>
+      <v-toolbar-title class="toolbar-option"><a>Register</a></v-toolbar-title>
+    </v-app-bar>
+    
+    <v-main>
+      <v-card id="main-container">
+        <v-card-title>Your contacts</v-card-title>
+        <div class="contact-container">
+          <Contact />
+          <Contact />
+          <Contact />
+          <Contact />
+          <Contact />
+          <Contact />
+          <Contact />
+        </div>
+      </v-card>
+    </v-main>
+
+    <v-footer app>
+        <span>&copy; 2020</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Contact from "./components/Contact.vue";
+
+export default Vue.extend(
+{
+  name: 'App',
+
+  components: {
+    Contact,
+  },
+  created() {
+    this.$vuetify.theme.dark = true;
+  }
+
+});
+</script>
+
+<style>
+#main-container {
+  padding: 1% 4.5%;
+  margin: 20px 10% 5% 10%;
+}
+
+.contact-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.toolbar-option {
+  margin-right: 1%;
+  font-weight: lighter;
+}
+</style>
+
+
+  
---
+++ src/components/Contact.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-card color="#26c6da" dark class="contact">
+        <v-card-title >Lore</v-card-title>
+        <v-card-text>+57 3455331234</v-card-text>
+    </v-card>
+</template>
+
+<script lang="ts">
+import {Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class Contact extends Vue {
+
+}
+</script>
+
+<style>
+.contact {
+    margin: 5px 0 0 5px;
+    width: 32.8%;
+}
+</style>
